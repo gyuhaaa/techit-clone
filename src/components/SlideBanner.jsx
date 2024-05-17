c054ca60e12d6919269573a161f024d9ffe0451e
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-17 16:19:59 +0900

    SlideBanner ing

--- src/components/SlideBanner.jsx
+++ src/components/SlideBanner.jsx
@@ -3,7 +3,7 @@ import Image1 from "/images/home_banner_1024_unity_1st.webp";
 const SlideBanner = () => {
   return (
     <div className="relative h-[400px]">
-      <div className="container-style text-white flex flex-col justify-center h-full items-center">
+      <div className="container-style flex flex-col justify-center h-full text-white">
         <h3 className="text-[32px] font-bold">유니티 게임 스쿨 1기</h3>
         <h5 className="mt-2">게임 개발자로 거듭나기 위한 6개월간의 여정</h5>
       </div>
