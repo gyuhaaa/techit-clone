d94e2124559483575c0ac29f98cb05ce2a213fa5
gyuhaaa <vkvkvk2892@gmail.com>
2024-05-17 15:50:44 +0900

    SlideBanner ing...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import TopBanner from "./components/TopBanner";
+import Header from "./components/Header";
+import SlideBanner from "./components/SlideBanner";
 
 const App = () => {
   const [isViewed, setIsViewed] = useState(false);
@@ -15,6 +17,8 @@ const App = () => {
   return (
     <div className="font-pretendard">
       {!isViewed && <TopBanner setIsViewed={setIsViewed} />}
+      <Header />
+      <SlideBanner />
     </div>
   );
 };
